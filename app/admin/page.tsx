3306f35d97eca40c3765cc6b8a238542e66aed51
Jaime M <jaimemejia1003@gmail.com>
2025-05-26 21:15:32 -0500

    feat: implement hero UI framework and add basic pages
    - Replace default Next.js template with Hero UI framework
    - Add basic pages for admin, evaluator, auth, and applicant routes
    - Simplify global CSS and layout configuration
    - Update dependencies to include Hero UI and related packages

---
+++ app/admin/page.tsx
@@ -0,0 +1,5 @@
+export default function page() {
+    return (
+        <div>El Admin papu</div>
+    )
+}
---
+++ app/applicant/page.tsx
@@ -0,0 +1,5 @@
+export default function page() {
+    return (
+        <div>Esta ruta no me convence</div>
+    )
+}
