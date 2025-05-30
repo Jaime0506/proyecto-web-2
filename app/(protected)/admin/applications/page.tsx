4d83a96a5dc670541a347cdba268b670930cedf3
Jaime M <jaimemejia1003@gmail.com>
2025-05-29 22:08:25 -0500

    local 2

--- app/(protected)/admin/applications/page.tsx
+++ app/(protected)/admin/applications/page.tsx
@@ -1,4 +1,12 @@
-export default function page() {
+import { getApplications } from "@/actions/admin/actions"
+
+export default async function ApplicationsPage() {
+    const { data: applications, error } = await getApplications()
+
+    if (error) {
+        return <div>No hay aplicaciones</div>;
+    }
+
     return (
         <div>page</div>
     )
